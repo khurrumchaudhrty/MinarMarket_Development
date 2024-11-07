8fe12ec813c0b99b7bf8191c03dfa5b8bdcb192a
AbdulAhad7553 <25100016@lums.edu.pk>
2024-11-07 17:54:16 +0500

    login corrected

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -8,7 +8,7 @@ function Login() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch('http://localhost:5000/signup', {
+        const response = await fetch('http://localhost:5000/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username, password })
