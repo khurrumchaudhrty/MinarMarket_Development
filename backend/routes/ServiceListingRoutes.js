7fd0752e39309c562f0df296ac4eeed9b8705743
SaadIlyas14 <158086775+SaadIlyas14@users.noreply.github.com>
2024-12-29 01:06:22 +0500

    Update ServiceListingRoutes.js

--- backend/routes/ServiceListingRoutes.js
+++ backend/routes/ServiceListingRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const {addServiceListing} = require("../controllers/ServiceListingController");
+const {addServiceListing, showServiceListings} = require("../controllers/ServiceListingController");
 
 // router.get('/',showServiceListings);
 router.post('/',addServiceListing);
+router.get('/',showServiceListings);
 
 module.exports = router;
