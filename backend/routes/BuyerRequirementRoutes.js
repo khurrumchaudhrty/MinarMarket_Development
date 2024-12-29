3d02d1660a010551585fd27641c23823d8425ab6
SaadIlyas14 <158086775+SaadIlyas14@users.noreply.github.com>
2024-12-29 20:40:12 +0500

    Use Case 9 Sprint 1

--- backend/routes/BuyerRequirementRoutes.js
+++ backend/routes/BuyerRequirementRoutes.js
@@ -2,10 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 
-const { listProduct } = require("../controllers/BuyerRequirementController");
-
+const { listProduct, myListings, deleteListings, updateListings } = require("../controllers/BuyerRequirementController");
 // Route for listing a product
 router.post("/", listProduct);
+router.get("/", myListings)
+
+router.post("/delete", deleteListings)
+router.post("/update", updateListings)
 
 // console.log("Route wala API: ", process.env.REACT_APP_API_URL+"/api/buyer-requirement")
 
