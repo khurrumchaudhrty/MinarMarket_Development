155a1869f05a2b3e8fd76421609c9fa186fba431
AniqaAqeel <aniqaaqeel6@gmail.com>
2024-12-29 13:22:25 +0500

    Comment out proposal-related routes in ProductListingRoutes

--- backend/routes/ProductListingRoutes.js
+++ backend/routes/ProductListingRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const {addProductListing,showProductListings, showMyProductListings, fetchProductListing, updateProductListing} = require('../controllers/ProductListingController');
-const { addProposal, getProposalsByBuyer, getProposalsBySeller } = require('../controllers/ProposalController');
+// const { addProposal, getProposalsByBuyer, getProposalsBySeller } = require('../controllers/ProposalController');
 
 // console.log("ITHAY AAN");
 router.post('/', addProductListing);
@@ -10,8 +10,8 @@ router.get('/',showProductListings);
 router.post('/buyer/my-product-listings', showMyProductListings)
 router.get('/fetch-product-details/:productId', fetchProductListing);
 router.put('/updateProduct/:productId', updateProductListing);
-router.post('/proposals', addProposal);
-router.post('/proposals/buyer', getProposalsByBuyer);
-router.post('/proposals/seller', getProposalsBySeller);
+// router.post('/proposals', addProposal);
+// router.post('/proposals/buyer', getProposalsByBuyer);
+// router.post('/proposals/seller', getProposalsBySeller);
 
 module.exports = router;
