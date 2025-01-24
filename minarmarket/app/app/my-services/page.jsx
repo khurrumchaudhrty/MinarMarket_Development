e35aa5124bf336ab77fe1b89540f84979170a69d
AniqaAqeel <aniqaaqeel6@gmail.com>
2025-01-24 20:46:10 +0500

    Update padding in My Products and My Services pages for improved layout

--- minarmarket/app/app/my-services/page.jsx
+++ minarmarket/app/app/my-services/page.jsx
@@ -68,7 +68,7 @@
                   <Header />
                   <div className="container flex-1 items-start md:grid md:grid-cols-[220px_1fr] md:gap-4 md:py-6">
                     <SidebarNav />
-                    <main className="flex-1 p-6">
+                    <main className="flex-1 px-6">
                         <div className="flex justify-between items-center mb-6">
                             <h1 className="text-2xl font-semibold">My Services</h1>
                             <Link href="/app/list-service" className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">List Service</Link>
