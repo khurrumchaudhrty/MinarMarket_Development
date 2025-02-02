0704409d574dbe53f7e4926258e6f6601c6b8dd5
AniqaAqeel <aniqaaqeel6@gmail.com>
2025-02-02 20:31:32 +0500

    Remove unused import from proposal form component

--- minarmarket/components/proposal-form.jsx
+++ minarmarket/components/proposal-form.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { getUserDetails } from "@/lib/SessionManager"
+
 
 import { useState } from "react";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
