dc0fef4b8d794e16bfbf77c378d739bee11f35e2
SaadIlyas14 <158086775+SaadIlyas14@users.noreply.github.com>
2025-03-09 17:11:19 +0500

    Update AdminListingRoutes.js

--- backend/routes/AdminListingRoutes.js
+++ backend/routes/AdminListingRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllProductListings, getUpdateProductListings } = require("../controllers/AdminListingController");
+const { getAllProductListings, getUpdateProductListings, getAllBuyerServiceRequirements, getAllBuyerProductRequirements, getAllServiceListings, getUpdateBuyerServiceListings, getUpdateBuyerProductListings, getUpdateServiceListings } = require("../controllers/AdminListingController");
 
 router.get("/", getAllProductListings);
+router.post("/seller-services", getAllServiceListings);
+router.post("/", getAllBuyerProductRequirements);
+router.put("/", getAllBuyerServiceRequirements);
 router.put("/update-listings-status", getUpdateProductListings);
+router.put("/seller-service", getUpdateServiceListings);
+router.put("/buyer-product", getUpdateBuyerProductListings);
+router.put("/buyer-service", getUpdateBuyerServiceListings);
 
 module.exports = router;
