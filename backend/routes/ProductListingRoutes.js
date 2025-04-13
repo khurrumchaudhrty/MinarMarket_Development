84afd88bf5cbab8472c58d63a999f7d344505306
SaadIlyas14 <158086775+SaadIlyas14@users.noreply.github.com>
2025-04-13 17:54:29 +0500

    Update ProductListingRoutes.js

--- backend/routes/ProductListingRoutes.js
+++ backend/routes/ProductListingRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const {addProductListing,showProductListings, showMyProductListings, fetchProductListing, updateProductListing, showProductCategoryListings, fetchLandingPageProducts, fetchCategoryLandingPage} = require('../controllers/ProductListingController');
+const {addProductListing,showProductListings, showMyProductListings, fetchProductListing, updateProductListing, showProductCategoryListings, fetchLandingPageProducts, fetchCategoryLandingPage, getRecommendedProducts} = require('../controllers/ProductListingController');
 const { addEmbedding } = require('../middleware/embeddingMiddleware');
 
 // console.log("ITHAY AAN");
@@ -13,6 +13,8 @@ router.put('/updateProduct/:productId', addEmbedding, updateProductListing);
 router.post('/fetch-category/:category', showProductCategoryListings);
 router.get('/fetch-landing-page-products', fetchLandingPageProducts);
 router.get('/fetch-category-landing-page/:category', fetchCategoryLandingPage);
+
+router.post('/recommendedproducts',getRecommendedProducts )
 // router.post('/proposals', addProposal);
 // router.post('/proposals/buyer', getProposalsByBuyer);
 // router.post('/proposals/seller', getProposalsBySeller);
